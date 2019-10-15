69356b28a78ccbb5f4ed5b9e6bec77378224c54e
PrivittJT2120 <56037868+PrivittJT2120@users.noreply.github.com>
2019-10-15 08:44:33 -0700

    Add files via upload

--- check.py
+++ check.py
@@ -1,35 +1,70 @@
 # This is a check/review to make sure nothing was "lost" over break
-# Name:
-# Period:
+
+# Julian Privitt
+# Period 1
 
 # Variable declaration and assignment
 # Example
-# Check
+myVar = "hello"
+# You try, declare two variables 1 string and 1 number, and assign values
+myNum = 7
+myFood = "Pasta"
 
-# While loops
+# While loop
 # Example
-# Check
-
-# String concatenation
+x = 10
+while x > 0:
+	print(x)
+	x = x - 1
+# You try, print name 100 times
+x = 0
+while x < 100:
+	print("Julian")
+	x = x + 1
+# String Concatenation
 # Example
-# Check
-
+name = "Donze"
+print("Hello " + name)
+# make a variable with your favorite movie
+# print "My favorite movie is "
+myMovie = "Ready Player One"
+print("My favorite movie is " + myMovie)
 # input
-# Example
-# Check
+# example
+myName = input("What is your name? ")
+print("Your name is " + myName)
+# prompt for favorite song and print "your favorite song is: "
+mySong = input("What is your favorite song? ")
+print("Your favorite song is " + mySong)\
+# Casting: Changing the type of a variable
+myNumber = 40
+print("My number is " + str(myNumber))
+num1 = input("Enter a number: ")
+num1 = int(num1) + 10
+print("num1 + 10 is " + str(num1))
 
-# casting
-# Example
-# Check
+# ask for two numbers, add them and print the answer
+numOne = input("What is your first number? ")
+numTwo = input("What is your second number? ")
+numOne = int(numOne)
+numTwo = int(numTwo)
+sumOne = int(numOne + numTwo)
+print("Your first number plus your second number is equal to " + str(sumOne))
 
-# Functions
-# Example
-# Check
+# if/else
+# example
+num1 = input("Type a number: ")
+num1 = int(num1)
+if num1 > 100:
+	print("Your number is more than 100.")
+elif num! == 100:
+	print("Your number is equal to 100")
+else:
+	print("Your number is less than 100.")
 
-# if/else statements
-# Example
-# Check
-
-# Create a random number
-# Example
-# Check
+# Ask if today is your birthday, if it is print happy birthday
+birthDay = input("Is today your birthday? ")
+if birthday == "Yes":
+	print("Happy Birthday! Go have fun.")
+else:
+	print("Can't wait till your birthday rolls around.")
